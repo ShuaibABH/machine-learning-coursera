4f8bc7598a61397863d343381a8d9651021d07e2
Shuaib Abubakker Bapputty Haji <42908782+ShuaibABH@users.noreply.github.com>
2019-05-23 04:46:43 -0700

    Update predictOneVsAll.m

--- Week 4 Assignments/Multi-class Classification and Neural Networks/mlclass-ex3/predictOneVsAll.m
+++ Week 4 Assignments/Multi-class Classification and Neural Networks/mlclass-ex3/predictOneVsAll.m
@@ -33,15 +33,9 @@ X = [ones(m, 1) X];
 %size(X)
 %size(all_theta)
 
-
-for i = 1:m
-    RX = repmat(X(i,:),num_labels,1);
-    RX = RX .* all_theta;
-    SX = sum(RX,2);
-    [val, index] = max(SX);
-    p(i) = index;
-end
-
+hp=all_theta*X';
+[val p]=max(hp);
+p=p';
 
 
 
